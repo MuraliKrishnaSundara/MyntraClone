a5a5ab1efff5018935b6d3c758e633479b01d847
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-07-21 19:26:43 +0530

    Update from murali(21/7/2021)

--- App/Myntra/app/src/main/java/com/example/myntra/StudioActivity.java
+++ App/Myntra/app/src/main/java/com/example/myntra/StudioActivity.java
@@ -7,8 +7,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class StudioActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<StudioData> studioDataList;
     private ImageView home;
     private ImageView categories;
     private ImageView studio;
@@ -21,6 +29,8 @@ public class StudioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studio);
         initViews();
+        buildIdentityList();
+        setRecyclerView();
         studio.setColorFilter(getResources().getColor(R.color.red));
         tvStudio.setTextColor(getResources().getColor(R.color.red));
         home.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +63,27 @@ public class StudioActivity extends AppCompatActivity {
         });
     }
 
+    private void buildIdentityList() {
+        studioDataList = new ArrayList<>();
+        StudioData studioData1 = new StudioData(R.drawable.p_image_1,"Rahul Karel", "23 hours ago", R.drawable.image_1,"25","Wear your #stripes with pride with this #blackandwhite\n#stripedkurta from #Hypernation. With my\n#ethniccollection constantly growing I am sometimes\nspoiled for choice but this #kurta is definitely one of my\nfavourites\n#festiveprintsformen #menkurtaset #mentrousers");
+        studioDataList.add(studioData1);
+        StudioData studioData2 = new StudioData(R.drawable.p_image_2,"Sarang Patil", "7 hours ago", R.drawable.image_2,"17","Looking for a #Party Wear in #Solid Hues ? #MANQ\n#Maroon #Slim Fit #PartyShirt fits in perfectly! Style it up\nwith a pair of #GreyChinos or #Black Trousers\n#partywear #oufit #menswear #fashion #partyoutfit\n#menshirt");
+        studioDataList.add(studioData2);
+        StudioData studioData3 = new StudioData(R.drawable.p_image_3,"Rhythm Sitlani", "7 hours ago", R.drawable.image_3,"68","#Buckethat is very in right now. #FabSeasons has come\nout with a collection of bucket hats in a variety of different\ncolours.\n#accesories #InTrend #solids #Typographic Tee\n#womentshirt");
+        studioDataList.add(studioData3);
+        StudioData studioData4 = new StudioData(R.drawable.p_image_4,"Irfan Khan Afridi", "8 hours ago", R.drawable.image_4,"25","This #INVICTUS #White #SlimFit #Stretch #FormalShirt\nis a must-have for any wardrobe. Go for a pair of darker\n#denim and comfy #loafers to keep it looking casually\ncool at work. #menshirt");
+        studioDataList.add(studioData4);
+        StudioData studioData5 = new StudioData(R.drawable.p_image_5,"Udita Sarkar", "11 hours ago", R.drawable.image_5,"27","A #solidcolored #croptop when paired with a pair of\n#Printedpants is always a fun look. This #Blue #Croptop\nfrom #CampusSutra is the perfect addition to my\nwardrobe and can be matched with anything\n#simpleisstylish #sportsbra #womentrousers");
+        studioDataList.add(studioData5);
+    }
+
+    private void setRecyclerView() {
+        StudioAdapter studioAdapter = new StudioAdapter(studioDataList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(studioAdapter);
+    }
+
     private void initViews() {
         home = findViewById(R.id.home);
         categories = findViewById(R.id.category);
@@ -60,5 +91,6 @@ public class StudioActivity extends AppCompatActivity {
         explore = findViewById(R.id.explore);
         profile = findViewById(R.id.profile);
         tvStudio = findViewById(R.id.tvCStudio);
+        recyclerView = findViewById(R.id.recyclerView);
     }
 }
---
+++ App/Myntra/app/src/main/java/com/example/myntra/StudioViewHolder.java
@@ -0,0 +1,44 @@
+package com.example.myntra;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StudioViewHolder extends RecyclerView.ViewHolder {
+
+    private ImageView mIvPImage;
+    private TextView mTvName;
+    private TextView mTvTime;
+    private ImageView mIvImage;
+    private TextView mTvLike;
+    private TextView mTvMessage;
+    private RelativeLayout relativeLayout;
+
+    public StudioViewHolder(@NonNull View itemView) {
+        super(itemView);
+        iniViews(itemView);
+    }
+
+    private void iniViews(View itemView) {
+        mIvPImage = itemView.findViewById(R.id.ivPImage);
+        mTvName = itemView.findViewById(R.id.tvName);
+        mTvTime = itemView.findViewById(R.id.tvTime);
+        mIvImage = itemView.findViewById(R.id.ivImage);
+        mTvLike = itemView.findViewById(R.id.tvHeartLike);
+        mTvMessage = itemView.findViewById(R.id.tvMessage);
+        relativeLayout = itemView.findViewById(R.id.relativeLayout);
+    }
+
+    public void setData(StudioData studioData) {
+        mIvPImage.setImageResource(studioData.getpImageView());
+        mTvName.setText(studioData.getName());
+        mTvTime.setText(studioData.getTime());
+        mIvImage.setImageResource(studioData.getImageView());
+        mTvLike.setText(studioData.getLike());
+        mTvMessage.setText(studioData.getMessage());
+    }
+}
