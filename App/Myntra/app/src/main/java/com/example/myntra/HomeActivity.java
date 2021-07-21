04427c544bc48d9e266f0eafd379700ec249ee2d
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-07-21 13:52:04 +0530

    Update from murali(21/7/2021)

--- App/Myntra/app/src/main/java/com/example/myntra/HomeActivity.java
+++ App/Myntra/app/src/main/java/com/example/myntra/HomeActivity.java
@@ -2,14 +2,35 @@ package com.example.myntra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button mBtnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initViews();
+        startActivities();
+    }
+
+    private void startActivities() {
+        mBtnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        mBtnProfile = findViewById(R.id.btnProfile);
     }
 
 }
