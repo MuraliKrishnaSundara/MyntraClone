5cf50e02d3675e876593ac9e7a4d7ef6a6ee78ab
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-07-22 17:58:04 +0530

    Update from murali (22/7/2021)

---
+++ App/Myntra/app/src/main/java/com/example/myntra/AutoScroll1Model.java
@@ -0,0 +1,18 @@
+package com.example.myntra;
+
+public class AutoScroll1Model {
+
+    private String imgUrl;
+
+    public AutoScroll1Model(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
